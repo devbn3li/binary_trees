c8442e2d7a089c0631a31b628ca5ef419d65be7e
devbn3li <fudwhc@gmail.com>
2023-11-02 00:16:05 +0200

    first commit

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,6 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
