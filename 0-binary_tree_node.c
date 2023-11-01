576a94ee7ef6482ca10031600d0a1cac9d11642b
devbn3li <fudwhc@gmail.com>
2023-11-02 00:18:07 +0200

    edit 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL;
+    binary_tree_t *new_node;
 
     new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
