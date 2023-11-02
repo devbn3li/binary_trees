852a793a594fd31548d133638c8a383d25692f0a
devbn3li <fudwhc@gmail.com>
2023-11-03 01:21:42 +0200

    betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,10 +6,10 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node)
-        return (0);
-    if (!node->parent)
-        return (1);
-    else
-        return (0);
+	if (!node)
+		return (0);
+	if (!node->parent)
+		return (1);
+	else
+		return (0);
 }
