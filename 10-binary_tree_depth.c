7eb91fdd56182ebda03410422c2f808a556d6823
devbn3li <fudwhc@gmail.com>
2023-11-03 01:46:31 +0200

    fffff

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,11 +7,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    if (tree == NULL)
-        return (0);
-    if (tree->parent)
-        depth = 1 + binary_tree_depth(tree->parent);
-    return (depth);
+	if (tree == NULL)
+		return (0);
+	if (tree->parent)
+		depth = 1 + binary_tree_depth(tree->parent);
+	return (depth);
 }
