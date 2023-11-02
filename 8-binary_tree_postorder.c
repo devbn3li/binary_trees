cc4a908741aac314edee37d9c207ccfd9b87934c
devbn3li <fudwhc@gmail.com>
2023-11-03 01:49:30 +0200

    ddd

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,12 +7,10 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
-		return;
-
-	binary_tree_inorder(tree->left, func);
-
-	binary_tree_inorder(tree->right, func);
-
-	(*func)(tree->n);
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
 }
