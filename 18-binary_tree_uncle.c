933b475b4c8ebb539ad4572927314dc55d46733d
devbn3li <fudwhc@gmail.com>
2023-11-03 02:22:44 +0200

    ggg

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node != NULL && node->parent != NULL)
-        return (binary_tree_sibling(node->parent));
-    return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
 }
